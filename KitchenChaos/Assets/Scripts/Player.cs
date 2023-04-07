0e7c71c0441d878d154bf2802a86023f93ad1a94
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-04-07 15:04:11 +0200

    New input system

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -7,16 +7,25 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
 
+    private PlayerInputActions playerInputActions;
     private Animator animator;
 
     private void Awake()
     {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+
         animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
