642f357d612f99540e983c2d72a2b446c13acc03
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-04-07 14:19:23 +0200

    Player controlls

---
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
+    }
+
+}
