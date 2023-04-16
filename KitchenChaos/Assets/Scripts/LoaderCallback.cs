76bcf3f3abbb546d94ec9add6cc5a3e9ab10c5f2
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-04-16 20:07:21 +0200

    Main menu

---
+++ KitchenChaos/Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Loader.LoaderCallback();
+    }
+
+}
